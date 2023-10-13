f29c85ada786c765b5b1d86f57bb89d0e93cc2ca
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-10-13 19:40:10 +0530

    Add: CORS middleware

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
 const app = express();
 const port = 3001;
+
+app.use(cors());
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);

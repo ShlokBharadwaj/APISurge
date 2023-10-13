4a65c7964788a7a0dc0596deb93fa73f98a413ec
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-10-13 20:05:52 +0530

    Update: Error handling retries count

--- client/src/components/UserDataTable.jsx
+++ client/src/components/UserDataTable.jsx
@@ -2,43 +2,61 @@ import React, { useState, useEffect } from 'react';
 
 const UserDataTable = () => {
     const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const maxRetries = 4;
+
+    const fetchData = async (retryCount) => {
+        try {
+            const response = await fetch('http://localhost:3001/api/users');
+            if (!response.ok) {
+                throw new Error('Failed to fetch data');
+            }
+            const jsonData = await response.json();
+            setData(jsonData);
+            setLoading(false);
+        } catch (err) {
+            if (retryCount > 0) {
+                setTimeout(() => fetchData(retryCount - 1), 2000);
+            } else {
+                setError('Failed to fetch data. Please try again later.');
+                setLoading(false);
+            }
+        }
+    };
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://localhost:3001/api/users');
-                const jsonData = await response.json();
-                setData(jsonData);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        fetchData();
+        fetchData(maxRetries);
     }, []);
 
     return (
         <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Username</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((user) => (
-                        <tr key={user.id}>
-                            <td>{user.id}</td>
-                            <td>{user.name}</td>
-                            <td>{user.username}</td>
-                            <td>{user.email}</td>
+            {loading ? (
+                <div>Loading...</div>
+            ) : error ? (
+                <div>Error: {error}</div>
+            ) : (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Username</th>
+                            <th>Email</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {data.map((user) => (
+                            <tr key={user.id}>
+                                <td>{user.id}</td>
+                                <td>{user.name}</td>
+                                <td>{user.username}</td>
+                                <td>{user.email}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </div>
     );
 };

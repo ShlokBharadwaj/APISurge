2b47ffe7ced6e9a613244feaf5ed50b8e9deb742
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-10-13 17:41:48 +0530

    Add: Routes, Error handling, Format JSON for API

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3001;
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+})
+
+app.get('/api/users', async (req, res) => {
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        const data = response.data;
+
+        const formattedData = data.map(user => ({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+        }));
+
+        res.json(formattedData);
+    } catch (error) {
+        console.error("API request error: ", error);
+        res.status(500).json({ error: "Error fetching data from the API" });
+    }
+});
